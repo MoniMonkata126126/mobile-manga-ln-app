a0cf66dbcb3abb4f5c2732fbdcb36ce65f5f73db
MoniMonkata126126 <simeon.k.simeonov06@gmail.com>
2024-11-11 13:59:26 +0200

    Base backend project in Spring Boot with java 17 and Maven

---
+++ backend/manga-ln-app/src/main/java/com/example/simeon/manga_ln_app/MangaLnAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.simeon.manga_ln_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaLnAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaLnAppApplication.class, args);
+	}
+
+}
---
+++ backend/manga-ln-app/src/test/java/com/example/simeon/manga_ln_app/MangaLnAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simeon.manga_ln_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaLnAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
