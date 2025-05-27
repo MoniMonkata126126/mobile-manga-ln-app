a0e597a64b587440757a4c46d00f70ab50b2829c
MoniMonkata126126 <simeon.k.simeonov06@gmail.com>
2025-05-27 17:39:55 +0300

    Changing env variable logic. Deleting env file and dotenv dependency

--- backend/manga-ln-app/src/main/java/com/example/simeon/manga_ln_app/MangaLnAppApplication.java
+++ backend/manga-ln-app/src/main/java/com/example/simeon/manga_ln_app/MangaLnAppApplication.java
@@ -1,6 +1,5 @@
 package com.example.simeon.manga_ln_app;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import  org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,14 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MangaLnAppApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-				.directory("src/main/resources")
-				.load();
-		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
-		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
-		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
-		System.setProperty("STORAGE_BUCKET_URL", dotenv.get("STORAGE_BUCKET_URL"));
-		System.setProperty("STORAGE_API_KEY", dotenv.get("STORAGE_API_KEY"));
 		SpringApplication.run(MangaLnAppApplication.class, args);
 	}
 
